baea2957de70aa66e479be4c194db8f85b185bad
sarnali15 <khatuna15-3515@diu.edu.bd>
2024-04-27 04:45:17 +0600

    get spots by id added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -27,7 +27,20 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        const spotsCollection = client.db('spotsDB').collection('spots')
+        const spotsCollection = client.db('spotsDB').collection('spots');
+
+        app.get('/spots', async (req, res) => {
+            const cursor = spotsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/spots/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await spotsCollection.findOne(query);
+            res.send(result)
+        })
 
         app.post('/spots', async (req, res) => {
             const newSpot = req.body;
