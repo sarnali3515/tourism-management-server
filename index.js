4f595c04c57f8f5410f8aeb53e40062f01f474b4
sarnali15 <khatuna15-3515@diu.edu.bd>
2024-04-27 01:06:26 +0600

    spot post added

--- index.js
+++ index.js
@@ -26,6 +26,17 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const spotsCollection = client.db('spotsDB').collection('spots')
+
+        app.post('/spots', async (req, res) => {
+            const newSpot = req.body;
+            console.log(newSpot);
+            const result = await spotsCollection.insertOne(newSpot);
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
