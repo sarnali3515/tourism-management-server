52ef040148d357f4a0a74cbdf51fd0774bbfea06
sarnali15 <khatuna15-3515@diu.edu.bd>
2024-04-28 03:14:05 +0600

    my list delete added

--- index.js
+++ index.js
@@ -56,6 +56,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/spots/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await spotsCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
 
         // user related apis
